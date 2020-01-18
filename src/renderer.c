6afc55a29502b25eb8a3f6190cd4d407c9a457e2
Vahagn <vhgn@pm.me>
2020-01-18 11:52:04 +0400

    Add renderer files

---
+++ src/renderer.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "renderer.h"
+	
+screen*
+init_screen
+	(unsigned width,
+	unsigned height,
+	char pixel)
+{
+	screen *screen_ref;
+	
+	screen_ref = malloc
+		(sizeof width +
+		sizeof height +
+		sizeof pixel * width * height);
+	
+	screen_ref->width = width;
+	screen_ref->height = height;
+
+	screen_ref->matrix = malloc(width * height);
+
+	for
+		(unsigned i = 0; i < height; i++)
+	{
+		for
+			(unsigned j = 0; j < width; j++)
+		{
+			set_screen_index(screen_ref, i, j, pixel);
+		}
+	}
+
+	return screen_ref;
+}
+
+void
+set_screen_index
+	(screen *screen_ref,
+	unsigned line_num,
+	unsigned col_num,
+	char pixel)
+{
+	char *line_ref,
+		*col_ref;
+
+	line_ref = screen_ref->matrix +
+		line_num * screen_ref->width;
+	
+	col_ref = line_ref + col_num;
+	*col_ref = pixel;
+
+	return;
+}
+
+char
+get_screen_index
+	(screen *screen_ref,
+	unsigned line_num,
+	unsigned col_num)
+{
+	char *line_ref,
+		*col_ref;
+
+	line_ref = screen_ref->matrix +
+		line_num * screen_ref->height;
+	
+	col_ref = line_ref + col_num;
+
+	return *col_ref;
+}
+
+void
+render_screen_matrix
+	(screen *screen_ref)
+{
+	for
+		(int i = 0; i < screen_ref->height; i++)
+	{
+		for
+			(int j = 0; j < screen_ref->width; j++)
+		{
+			char pixel = get_screen_index(screen_ref, i, j);
+			printf("%c", pixel);
+		}
+		printf("\n");
+	}
+
+	return;
+}
---
+++ src/renderer.h
@@ -0,0 +1,69 @@
+/**
+ * 
+ * LICENSE: MIT
+ * 
+*/
+
+#ifndef _RENDERER
+#define _RENDERER
+
+struct
+screen
+{
+	/** 
+	 * Width of the matrix
+	 */
+	unsigned width;
+	
+	/** 
+	 * Height of the matrix
+	 */
+	unsigned height;
+
+	/** 
+	 * Matrix of chacaters
+	 */
+	char *matrix;
+};
+
+/**
+ * Screen with matrix of pixels
+*/
+typedef struct screen screen;
+
+/** 
+ * Initialize a screen with char[width][height] then fill
+ */
+screen*
+init_screen
+	(unsigned width,
+	unsigned height,
+	char pixel);
+
+/** 
+ * Set the screen's [line_num][col_num] to pixel
+ */
+void
+set_screen_index
+	(screen *screen_ref,
+	unsigned line_num,
+	unsigned col_num,
+	char pixel);
+
+/** 
+ * Get the screen's [line_num][col_num]
+ */
+char
+get_screen_index
+	(screen *screen_ref,
+	unsigned line_num,
+	unsigned col_num);
+
+/** 
+ * Render the screen's all characters
+ */
+void
+render_screen_matrix
+	(screen *screen_ref);
+
+#endif

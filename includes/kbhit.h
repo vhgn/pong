8bd7ba872a634337b3a435b2558816052d9c2f07
Vahagn <vhgn@pm.me>
2020-01-20 20:19:53 +0400

    Add kbhit for POSIX

---
+++ includes/kbhit.h
@@ -0,0 +1,7 @@
+/**
+ Linux (POSIX) implementation of _kbhit().
+ Morgan McGuire, morgan@casual-effectcs.com
+ */
+
+int
+_kbhit();
---
+++ sources/kbhit.c
@@ -0,0 +1,30 @@
+/**
+ Linux (POSIX) implementation of _kbhit().
+ Morgan McGuire, morgan@casual-effectcs.com
+ */
+#include <stdio.h>
+#include <sys/ioctl.h>
+#include <sys/select.h>
+#include <termios.h>
+
+int
+_kbhit()
+{
+	static const int STDIN = 0;
+	static char initialized = 0;
+
+	if(!initialized)
+	{
+		struct termios term;
+		tcgetattr(STDIN, &term);
+		term.c_lflag &= ~ICANON;
+		term.c_lflag &= ~ECHO;
+		tcsetattr(STDIN, TCSANOW, &term);
+		setbuf(stdin, NULL);
+		initialized = 1;
+	}
+
+	int bytesWaiting;
+	ioctl(STDIN, FIONREAD, &bytesWaiting);
+	return bytesWaiting;
+}

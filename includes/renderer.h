3b74e64f4932b8692062dc32cd8b22049bb04427
Vahagn <vhgn@pm.me>
2020-01-20 20:41:38 +0400

    Refactor naming, comments, remove typedef

--- includes/renderer.h
+++ includes/renderer.h
@@ -4,66 +4,64 @@
  * 
  */
 
-#ifndef _RENDERER
-#define _RENDERER
+#ifndef _RENDERER_H
+#define _RENDERER_H
 
+/**
+ * Screen with matrix of pixels
+ */
 struct
 screen
 {
-	/** 
+	/**
 	 * Width of the matrix
 	 */
 	unsigned width;
 
-	/** 
+	/**
 	 * Height of the matrix
 	 */
 	unsigned height;
 
-	/** 
+	/**
 	 * Matrix of chacaters
 	 */
 	char *matrix;
 };
 
 /**
- * Screen with matrix of pixels
- */
-typedef struct screen screen;
-
-/** 
  * Initialize a screen with char[width][height] then fill
  */
-screen*
+struct screen*
 init_screen
 	(unsigned width,
 	unsigned height,
 	char pixel);
 
-/** 
+/**
  * Set the screen's [line_num][col_num] to pixel
  */
 void
 set_screen_index
-	(screen *screen_ref,
+	(struct screen *screen_ref,
 	unsigned line_num,
 	unsigned col_num,
 	char pixel);
 
-/** 
+/**
  * Get the screen's [line_num][col_num]
  */
 char
 get_screen_index
-	(screen *screen_ref,
+	(struct screen *screen_ref,
 	unsigned line_num,
 	unsigned col_num);
 
-/** 
+/**
  * Render the screen's all characters
  */
 void
 render_screen_matrix
-	(screen *screen_ref);
+	(struct screen *screen_ref);
 
 #endif
